f1ce3f6817601676e4f0ffb228509d76ec50f201
jangdongguk <jangdong456@gmail.com>
2024-09-13 09:26:11 +0900

    암호화 패스워드 일치 테스트

--- src/test/java/com/jang/app/members/MemberServiceTest.java
+++ src/test/java/com/jang/app/members/MemberServiceTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 public class MemberServiceTest {
 	@Autowired
@@ -15,14 +18,35 @@ public class MemberServiceTest {
 	private PasswordEncoder passwordEncoder;
 	
 	@Test
-	void test() throws Exception {
+	void passwordUpdateTest() throws Exception {
+		// 기존 admin admin -> 새로운 비번 :1234
 		MemberVO memberVO = new MemberVO();
+		memberVO.setUsername("admin");
+		memberVO.setPassword("admin");
+		String newpassword = "1234";
 		
-		memberVO.setUsername("user");
-		memberVO.setPassword(passwordEncoder.encode("user"));
+		MemberVO check = memberMapper.detail(memberVO);
 		
-		int result = memberMapper.pwUpdate(memberVO);
+		log.info("1.MemberVO : {}", memberVO);
+		log.info("2.Check : {}", check);
 		
-		assertEquals(1, result);
+		if(passwordEncoder.matches(memberVO.getPassword(), check.getPassword())) {
+			log.info("3. 패스워드 일치");
+		}
+		
+		
+		assertEquals(check.getPassword(), memberVO.getPassword());
 	}
+	
+//	@Test
+//	void test() throws Exception {
+//		MemberVO memberVO = new MemberVO();
+//		
+//		memberVO.setUsername("user");
+//		memberVO.setPassword(passwordEncoder.encode("user"));
+//		
+//		int result = memberMapper.pwUpdate(memberVO);
+//		
+//		assertEquals(1, result);
+//	}
 }
